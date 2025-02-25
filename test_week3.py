3e31a69467a50afd28b6a032dd750b2073e3be85
Arya Boudaie <arya@aryaboudaie.com>
2025-02-25 09:59:59 -0800

    Initial

---
+++ test_week3.py
@@ -0,0 +1,426 @@
+try:
+    import week3_answers as week3
+except ImportError:
+    import week3
+
+import unittest
+
+class TestTimeout(Exception):
+    pass
+
+
+# LL Class
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+def convert_array_to_ll(arr):
+    if not arr:
+        return None
+    head = Node(arr[0])
+    cur = head
+    for item in arr[1:]:
+        cur.next_node = Node(item)
+        cur = cur.next_node
+    return head
+
+def convert_ll_to_array(head):
+    arr = []
+    cur = head
+    while cur is not None:
+        arr.append(cur.data)
+        cur = cur.next_node
+    return arr
+
+
+class TestItemInLL(unittest.TestCase):
+    def test_item_in_ll(self):
+        head = convert_array_to_ll([1, 2, 3, 4, 5])
+        self.assertTrue(week3.is_item_in_ll(head, 3))
+
+    def test_item_not_in_ll(self):
+        head = convert_array_to_ll([1, 2, 3, 4, 5])
+        self.assertFalse(week3.is_item_in_ll(head, 6))
+
+    def test_empty_ll(self):
+        head = convert_array_to_ll([])
+        self.assertFalse(week3.is_item_in_ll(head, 6))
+
+
+class TestReverseLL(unittest.TestCase):
+    def test_reverse_ll(self):
+        head = convert_array_to_ll([1, 2, 3, 4, 5])
+        head = week3.reverse_ll(head)
+        self.assertListEqual(convert_ll_to_array(head), [5, 4, 3, 2, 1])
+
+    def test_reverse_ll_empty(self):
+        head = convert_array_to_ll([])
+        head = week3.reverse_ll(head)
+        self.assertListEqual(convert_ll_to_array(head), [])
+
+    def test_reverse_ll_one(self):
+        head = convert_array_to_ll([1])
+        head = week3.reverse_ll(head)
+        self.assertListEqual(convert_ll_to_array(head), [1])
+
+
+class TestInterleaveLLs(unittest.TestCase):
+    def test_interleave_lls(self):
+        head1 = convert_array_to_ll([1, 2, 3])
+        head2 = convert_array_to_ll([4, 5, 6])
+        head = week3.interleave_lls(head1, head2)
+        self.assertListEqual(convert_ll_to_array(head), [1, 4, 2, 5, 3, 6])
+
+    def test_interleave_lls_one(self):
+        head1 = convert_array_to_ll([1])
+        head2 = convert_array_to_ll([2])
+        head = week3.interleave_lls(head1, head2)
+        self.assertListEqual(convert_ll_to_array(head), [1, 2])
+
+
+class TestRemoveItem(unittest.TestCase):
+    def test_remove_item(self):
+        head = convert_array_to_ll([1, 2, 3, 2, 4])
+        head = week3.remove_item(head, 2)
+        self.assertListEqual(convert_ll_to_array(head), [1, 3, 4])
+
+
+class TestRemoveIndex(unittest.TestCase):
+    def test_remove_index(self):
+        head = convert_array_to_ll([1, 2, 3, 2, 4])
+        head = week3.remove_index(head, 2)
+        self.assertListEqual(convert_ll_to_array(head), [1, 2, 2, 4])
+
+class TestRemoveHead(unittest.TestCase):
+    def test_remove_head(self):
+        head = convert_array_to_ll([1, 2, 3, 2, 4])
+        head = week3.remove_head(head)
+        self.assertListEqual(convert_ll_to_array(head), [2, 3, 2, 4])
+
+    def test_remove_head_size1(self):
+        head = convert_array_to_ll([1])
+        head = week3.remove_head(head)
+        self.assertListEqual(convert_ll_to_array(head), [])
+
+    def test_remove_head_size2(self):
+        head = convert_array_to_ll([1, 2])
+        head = week3.remove_head(head)
+        self.assertListEqual(convert_ll_to_array(head), [2])
+
+
+class TestRemoveTail(unittest.TestCase):
+    def test_remove_tail(self):
+        head = convert_array_to_ll([1, 2, 3, 2, 4])
+        head = week3.remove_tail(head)
+        self.assertListEqual(convert_ll_to_array(head), [1, 2, 3, 2])
+
+    def test_remove_tail_size1(self):
+        head = convert_array_to_ll([1])
+        head = week3.remove_tail(head)
+        self.assertListEqual(convert_ll_to_array(head), [])
+
+    def test_remove_tail_size2(self):
+        head = convert_array_to_ll([1, 2])
+        head = week3.remove_tail(head)
+        self.assertListEqual(convert_ll_to_array(head), [1])
+
+
+class TestRemoveDuplicates(unittest.TestCase):
+    def test_remove_dups(self):
+        head = convert_array_to_ll([1, 2, 2, 3, 2, 3, 5])
+        head = week3.remove_duplicates(head)
+        self.assertListEqual(convert_ll_to_array(head), [1, 2, 3, 5])
+
+    def test_remove_dups_empty(self):
+        head = convert_array_to_ll([])
+        head = week3.remove_duplicates(head)
+        self.assertListEqual(convert_ll_to_array(head), [])
+
+    def test_remove_dups_one(self):
+        head = convert_array_to_ll([1])
+        head = week3.remove_duplicates(head)
+        self.assertListEqual(convert_ll_to_array(head), [1])
+
+    def test_remove_dups_two(self):
+        head = convert_array_to_ll([1, 2])
+        head = week3.remove_duplicates(head)
+        self.assertListEqual(convert_ll_to_array(head), [1, 2])
+
+    def test_remove_dups_two_dups(self):
+        head = convert_array_to_ll([1, 1])
+        head = week3.remove_duplicates(head)
+        self.assertListEqual(convert_ll_to_array(head), [1])
+
+# Binary Tree questions will use the following definition of a Tree:
+class BST(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None  # Left child, in a BST every node in the left subtree is less than the current node
+        self.right = None  # Right child, in a BST every node in the right subtree is greater than or equal to the current node
+
+def convert_bst_to_array(root):
+    """
+    Return array where A[1] is the root and A[i*2] is the left child of i and A[i*2+1] is the right child of i
+    """
+    arr = [None]
+    q = [root]
+    while q:
+        node = q.pop(0)
+        if node is None:
+            arr.append(None)
+        else:
+            arr.append(node.data)
+            q.append(node.left)
+            q.append(node.right)
+    return arr
+
+
+def convert_array_to_bst(arr):
+    """
+    Convert array to BST where A[1] is the root and A[i*2] is the left child of i and A[i*2+1] is the right child of i
+    """
+    if not arr:
+        return None
+    root = BST(arr[1])
+    q = [root]
+    i = 2
+    while i < len(arr):
+        node = q.pop(0)
+        if arr[i] is not None:
+            node.left = BST(arr[i])
+            q.append(node.left)
+        i += 1
+        if i < len(arr) and arr[i] is not None:
+            node.right = BST(arr[i])
+            q.append(node.right)
+        i += 1
+    return root
+
+class TestIsTreeBST(unittest.TestCase):
+    def test_is_tree_bst(self):
+        root = BST(8)
+        root.left = BST(4)
+        root.right = BST(12)
+        root.left.left = BST(3)
+        root.left.right = BST(6)
+        root.right.left = BST(9)
+        root.right.right = BST(15)
+        self.assertTrue(week3.is_tree_bst(root))
+
+    def test_is_tree_bst_array(self):
+        root = convert_array_to_bst([None, 8, 4, 12, 3, 6, 9, 15])
+        self.assertTrue(week3.is_tree_bst(root))
+
+    def test_is_tree_bst_empty(self):
+        root = BST(None)
+        self.assertTrue(week3.is_tree_bst(root))
+
+    def test_is_tree_bst_one(self):
+        root = BST(1)
+        self.assertTrue(week3.is_tree_bst(root))
+
+    def test_is_tree_bst_false(self):
+        root = convert_array_to_bst([None, 1, 2, 3, 4, 5, 6, 7])
+        root.left.data = 10
+        self.assertFalse(week3.is_tree_bst(root))
+
+class SearchBinaryTree(unittest.TestCase):
+    def setUp(self):
+        self.binary_tree = BST(1)
+        self.binary_tree.left = BST(2)
+        self.binary_tree.right = BST(3)
+        self.binary_tree.left.left = BST(4)
+        self.binary_tree.left.right = BST(5)
+        self.binary_tree.right.left = BST(6)
+        self.binary_tree.right.right = BST(7)
+
+    def test_find_in_binary_tree(self):
+        self.assertTrue(week3.search_binary_tree(self.binary_tree, 5))
+    def test_not_in_binary_tree(self):
+        self.assertFalse(week3.search_binary_tree(self.binary_tree, 10))
+
+    def test_find_in_binary_tree_array(self):
+        root = convert_array_to_bst([None, 8, 4, 12, 3, 6, 9, 15])
+        self.assertTrue(week3.search_binary_tree(root, 9))
+
+    def test_find_in_binary_tree_one(self):
+        root = BST(1)
+        self.assertTrue(week3.search_binary_tree(root, 1))
+
+    def test_find_in_binary_tree_one_false(self):
+        root = BST(1)
+        self.assertFalse(week3.search_binary_tree(root, 21))
+
+def balance_tree(items, start=0, end=None):
+    if not items:
+        return None
+
+    stack = []
+    mid = len(items) // 2
+    root = BST(items[mid])
+    stack.append((root, 0, len(items)))
+
+    while stack:
+        node, start, end = stack.pop()
+        mid = (start + end) // 2
+
+        if start < mid:
+            left_mid = (start + mid) // 2
+            node.left = BST(items[left_mid])
+            stack.append((node.left, start, mid))
+
+        if mid + 1 < end:
+            right_mid = (mid + 1 + end) // 2
+            node.right = BST(items[right_mid])
+            stack.append((node.right, mid + 1, end))
+
+    return root
+
+class TestSearchBST(unittest.TestCase):
+    def test_search_bst(self):
+        root = convert_array_to_bst([None, 8, 4, 12, 3, 6, 9, 15])
+        self.assertTrue(week3.search_bst(root, 9))
+
+    def test_search_bst_false(self):
+        root = convert_array_to_bst([None, 8, 4, 12, 3, 6, 9, 15])
+        self.assertFalse(week3.search_bst(root, 10))
+
+    def test_search_bst_one(self):
+        root = BST(1)
+        self.assertTrue(week3.search_bst(root, 1))
+
+    def test_search_bst_one_false(self):
+        root = BST(1)
+        self.assertFalse(week3.search_bst(root, 21))
+
+    def test_search_bst_large(self):
+        tree = balance_tree(list(range(1, 2**20 + 1)))
+        self.assertTrue(week3.search_bst(tree, 500_000))
+        self.assertFalse(week3.search_bst(tree, 20_000_001))
+
+class TestInsertBST(unittest.TestCase):
+    def test_insert_bst(self):
+        root = convert_array_to_bst([None, 8, 4, 12, 3, 6, 9, 15])
+        root = week3.insert_bst(root, 5)
+        self.assertEqual(root.left.right.left.data, 5)
+
+    def test_insert_bst_left(self):
+        root = convert_array_to_bst([None, 10])
+        root = week3.insert_bst(root, 5)
+        self.assertEqual(root.left.data, 5)
+
+    def test_insert_bst_right(self):
+        root = convert_array_to_bst([None, 10])
+        root = week3.insert_bst(root, 15)
+        self.assertEqual(root.right.data, 15)
+
+    def test_insert_into_middle(self):
+        root = convert_array_to_bst([None, 8, 1, 100])
+        root = week3.insert_bst(root, 7)
+        self.assertEqual(root.left.right.data, 7)
+
+
+class TestBSTMin(unittest.TestCase):
+    def test_bst_min(self):
+        root = convert_array_to_bst([None, 8, 4, 12, 3, 6, 9, 15])
+        self.assertEqual(week3.bst_min(root), 3)
+
+    def test_bst_min_unbalanced_tree(self):
+        root = BST(8)
+        root.right = BST(12)
+        root.right.right = BST(15)
+        self.assertEqual(week3.bst_min(root), 8)
+
+
+class TestBSTMax(unittest.TestCase):
+    def test_bst_max(self):
+        root = convert_array_to_bst([None, 8, 4, 12, 3, 6, 9, 15])
+        self.assertEqual(week3.bst_max(root), 15)
+
+    def test_bst_max_unbalanced_tree(self):
+        root = BST(8)
+        root.left = BST(4)
+        root.left.left = BST(3)
+        self.assertEqual(week3.bst_max(root), 8)
+
+
+class TestBFS(unittest.TestCase):
+    graph = {
+        1: [2, 3],
+        2: [1, 4],
+        3: [1, 4],
+        4: [2, 3]
+    }
+    def test_bfs(self):
+        self.assertListEqual(week3.bfs(self.graph, 1), [1, 2, 3, 4])
+
+    def test_bfs_from2(self):
+        self.assertListEqual(week3.bfs(self.graph, 2), [2, 1, 4, 3])
+
+    def test_bfs_one_val(self):
+        self.assertListEqual(week3.bfs({1: []}, 1), [1])
+
+    def test_bfs_two_neighbors(self):
+        self.assertListEqual(week3.bfs({1: [2], 2: [1]}, 1), [1, 2])
+
+
+class TestPathExists(unittest.TestCase):
+    graph = {
+        1: [2],
+        2: [1, 4],
+        3: [1, 4],
+        4: [2, 3],
+        5: [6],
+        6: [5]
+    }
+    def test_path_exists(self):
+        self.assertTrue(week3.path_exists(self.graph, 1, 4))
+
+    def test_path_exists_false(self):
+        self.assertFalse(week3.path_exists(self.graph, 1, 5))
+
+    def test_path_exists_self(self):
+        self.assertTrue(week3.path_exists(self.graph, 1, 1))
+
+    def test_path_exists_one(self):
+        self.assertTrue(week3.path_exists({1: []}, 1, 1))
+
+    def test_path_exists_two_neighbors(self):
+        self.assertTrue(week3.path_exists({1: [2], 2: [1]}, 1, 2))
+
+class TestShortestPath(unittest.TestCase):
+    graph = {
+        1: [2],
+        2: [1, 4],
+        3: [1, 4],
+        4: [2, 3],
+        5: [6],
+        6: [5]
+    }
+    def test_shortest_path(self):
+        self.assertListEqual(week3.shortest_path(self.graph, 1, 4), [1, 2, 4])
+
+    def test_shortest_path_one_away(self):
+        self.assertListEqual(week3.shortest_path(self.graph, 1, 2), [1, 2])
+
+    def test_shortest_path_self(self):
+        self.assertListEqual(week3.shortest_path(self.graph, 1, 1), [1])
+
+    def test_no_path(self):
+        self.assertListEqual(week3.shortest_path(self.graph, 1, 5), [])
+
+    def test_path_one(self):
+        self.assertListEqual(week3.shortest_path({1: []}, 1, 1), [1])
+
+    def test_path_exists_two_neighbors(self):
+        self.assertListEqual(week3.shortest_path({1: [2], 2: [1]}, 1, 2), [1, 2])
+
+    def test_large(self):
+        graph = {i: [i+1] for i in range(100000)}
+        graph[99997].append(100000)
+        graph[100000] = [0]
+        self.assertListEqual(week3.shortest_path(graph, 0, 100000), list(range(99998)) + [100000])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ week3.py
@@ -0,0 +1,208 @@
+# LinkedList questions
+
+# LinkedList questions will use the following definition of a LinkedList:
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+# Example
+def print_all_nodes(head):
+    cur = head
+    while cur is not None:
+        print(cur.data)
+        cur = cur.next_node
+
+# Or recursively
+def print_all_nodes_recursive(head):
+    if head is None:
+        return
+    print(head.data)
+    print_all_nodes(head.next_node)
+
+
+def is_item_in_ll(head, item):
+    """
+    Return true if item is in the linked list starting at head, otherwise return false
+    """
+    return False
+
+
+def remove_head(head):
+    """
+    Given the head of a linked list, remove the first item from the linked list
+    """
+    return None
+
+
+def reverse_ll(head):
+    """
+    Given the head of a linked list, return the head of the linked list reversed
+    """
+    return None
+
+
+def interleave_lls(head1, head2):
+    """
+    Given the heads of two linked lists, interleave the two linked lists and return the head of the new linked list
+    E.g. if the first linked list is 1 -> 2 -> 3 and the second linked list is 4 -> 5 -> 6, the result should be
+    1 -> 4 -> 2 -> 5 -> 3 -> 6
+
+    head1 and head2 will never be None. The two linked lists will be the same length for this, but try to write your
+    code in a way that they do not have to be the same length for a challenge :)
+    """
+    return None
+
+def remove_item(head, item):
+    """
+    Given the head of a linked list and an item, remove all instances of the item from the linked list
+    E.g. if the list is 1 -> 2 -> 3 -> 2 -> 4 and the item is 2, the list should become 1 -> 3 -> 4
+    The item will never be at the start or end of the list but you can challenge yourself to do that
+    """
+    return None
+
+def remove_index(head, index):
+    """
+    Given the head of a linked list and an index, remove the item at the index from the linked list
+    E.g. if the list is 1 -> 2 -> 3 -> 2 -> 4 and the index is 2, the list should become 1 -> 2 -> 2 -> 4
+    The item will never be at the start or end of the list but you can challenge yourself to support that
+    """
+    return None
+
+
+def remove_tail(head):
+    """
+    Given the head of a linked list, remove the last item from the linked list and return the head
+    Remember, the list may only have one, two, or more items.
+    """
+    return None
+
+
+def remove_duplicates(head):
+    """
+    Given the head of a linked list, remove all duplicate items from the linked list
+    E.g. if the list is 1 -> 2 -> 3 -> 2 -> 4, the list should become 1 -> 2 -> 3 -> 4
+    """
+    return None
+
+
+# Binary Tree questions
+
+# Binary Tree questions will use the following definition of a Tree:
+class BST(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None  # Left child, in a BST every node in the left subtree is less than the current node
+        self.right = None  # Right child, in a BST every node in the right subtree is greater than or equal to the current node
+
+# Example
+def print_all_nodes_bst(root):
+    if root is None:
+        return
+    print_all_nodes_bst(root.left)
+    print(root.data)
+    print_all_nodes_bst(root.right)
+
+
+def is_tree_bst(root):
+    """
+    Given the root of a binary tree, return True if the tree is a binary search tree, otherwise return False
+    A binary tree is a BST if for all nodes, the left subtree is less than the current node and the right subtree is greater than or equal to the current node.
+    Hint pseudocode:
+        1. If root is None, then it is an empty BST. Return True
+        2. Check if root follows the BST property where left is less than root and right is greater than or equal to root
+        3. Recursively check the left and right subtrees
+    """
+    return False
+
+
+def search_binary_tree(root, data):
+    """
+    Given the root of a binary tree and an integer, return True if the integer is in the tree, otherwise return False.
+    Note: The tree is NOT a binary search tree. So you have to search the entire tree.
+    """
+    return False
+
+
+def search_bst(root, data):
+    """
+    Given the root of a binary search tree and an integer, return True if the integer is in the tree, otherwise return False.
+    Your algorithm should be log(n) time complexity, aka do NOT search the whole tree.
+    """
+    return False
+
+
+def insert_bst(root, data):
+    """
+    Given the root of a binary search tree and an integer, insert the integer into the tree and return the root.
+    Hint: Might be easier to do this one iteratively. Look for the correct place to insert the node and insert it there.
+    But recursively is possible.
+    """
+    return None
+
+
+def bst_min(root):
+    """
+    Given the root of a binary search tree, return the minimum value in the tree. Tree will not be empty.
+    Hint: Where would this item be?
+    """
+    return -1
+
+
+def bst_max(root):
+    """
+    Given the root of a binary search tree, return the minimum value in the tree. Tree will not be empty.
+    Hint: Where would this item be?
+    """
+    return -1
+
+# Graph Problems
+# Graphs will be represented as a dictionary where the key is the node and the value is a list of neighbors
+# Example Corresponds to this graph:
+# # 1 -- 2
+# # |    |
+# # 3 -- 4
+graph = {
+    1: [2, 3],
+    2: [1, 4],
+    3: [1, 4],
+    4: [2, 3]
+}
+
+def bfs(graph, start):
+    """
+    Given a graph and a starting node, return a list of nodes visited in breadth first order.
+    Hint: You can use a python array as a queue; queue = []
+    To add to the end of the queue: queue.append(item)
+    To remove from the front of the queue: queue.pop(0)
+    Hint 2: Make sure you use a visited set to keep track of nodes you have visited so you do not enter cycles.
+    """
+    return []
+
+
+def shortest_path(graph, start, end):
+    """
+    Given a graph, a starting node, and an ending node, return the shortest path from the starting node to the ending node.
+    If there is no path, return an empty list.
+    Note, this is a breadth first search problem. The code will look similar to the above code, but now you have to keep track of the paths.
+    The shortest path is the one you will find after doing a BFS starting from the start node.
+
+    Pseudocode:
+    1. Initialize a queue with just the partial path [start] and a visited set
+    2. While the queue is not empty:
+        a. Pop the first path from the queue
+        b. Get the last node from the path - that is your current node
+        c. If the last node is the end node, return the path
+        d. If the last node has not been visited:
+            i. Add the node to the visited set
+            ii. For each neighbor of the last node:
+                1. Create a new path by appending the neighbor to the path (new_path = path + [neighbor])
+                2. Add the new path to the queue
+    3. If you finish the while loop, return an empty list
+    """
+    visited = set()
+    queue = [[start]]  # Queue will hold all the paths you've seen so far.
+
+
+    # The rest is for you to do...
+    return []
